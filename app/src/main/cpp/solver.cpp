fcd71c3cd723e63fa915c7aa74f407c8fb8560a5
aryav106-sketch <aryav106@gmail.com>
2026-05-22 19:48:35 +0530

    Update solver.cpp

--- app/src/main/cpp/solver.cpp
+++ app/src/main/cpp/solver.cpp
@@ -1,18 +1,29 @@
 #include "solver.h"
+#include "dimacs_parser.h"
 
 Solver::Solver() {
+
     variables = 0;
     clauses = 0;
 }
 
-bool Solver::solve(const std::string& cnf) {
-    return parseCNF(cnf) && dpll();
-}
-
 bool Solver::parseCNF(const std::string& cnf) {
-    return true;
+
+    return DimacsParser().parse(cnf);
 }
 
 bool Solver::dpll() {
+
     return true;
+}
+
+bool Solver::solve(const std::string& cnf) {
+
+    bool valid = parseCNF(cnf);
+
+    if (!valid) {
+        return false;
+    }
+
+    return dpll();
 }

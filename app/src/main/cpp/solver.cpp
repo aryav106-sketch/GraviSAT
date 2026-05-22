e44671a20bfc6fc48a619f015d6eb6ff9826e332
aryav106-sketch <aryav106@gmail.com>
2026-05-22 19:26:38 +0530

    Update solver.cpp

--- app/src/main/cpp/solver.cpp
+++ app/src/main/cpp/solver.cpp
@@ -1,56 +1,18 @@
 #include "solver.h"
-#include "dimacs_parser.h"
-#include "watcher.h"
-#include "vsids.h"
-#include "analyzer.h"
 
-Solver::Solver()
-        : variables(0),
-          clauses(0) {
-}
-
-bool Solver::parseCNF(const std::string& cnf) {
-
-    return DimacsParser::validate(cnf);
-}
-
-bool Solver::dpll() {
-
-    Watcher watcher;
-
-    watcher.addWatch(1);
-
-    bool propagation = watcher.propagate();
-
-    if (!propagation) {
-
-        Analyzer analyzer;
-
-        return analyzer.analyzeConflict();
-    }
-
-    VSIDS heuristic;
-
-    heuristic.bumpActivity(1);
-
-    int variable = heuristic.selectVariable();
-
-    if (variable == -1) {
-
-        return false;
-    }
-
-    return true;
+Solver::Solver() {
+    variables = 0;
+    clauses = 0;
 }
 
 bool Solver::solve(const std::string& cnf) {
+    return parseCNF(cnf) && dpll();
+}
 
-    bool parsed = parseCNF(cnf);
+bool Solver::parseCNF(const std::string& cnf) {
+    return true;
+}
 
-    if (!parsed) {
-
-        return false;
-    }
-
-    return dpll();
+bool Solver::dpll() {
+    return true;
 }

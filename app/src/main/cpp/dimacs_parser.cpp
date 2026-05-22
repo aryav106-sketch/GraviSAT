8894c1268f7ceb4dbbf5e29aa9baae40a2b1588c
aryav106-sketch <aryav106@gmail.com>
2026-05-22 16:11:23 +0530

    Update dimacs_parser.cpp

--- app/src/main/cpp/dimacs_parser.cpp
+++ app/src/main/cpp/dimacs_parser.cpp
@@ -1,52 +1,13 @@
-#include "dimacs_parser.h"
-#include <fstream>
-#include <sstream>
+#ifndef GRAVISAT_DIMACS_PARSER_H
+#define GRAVISAT_DIMACS_PARSER_H
 
-CNFFormula parseDIMACS(const std::string& filename) {
+#include <string>
 
-    CNFFormula formula;
-    formula.variables = 0;
+class DimacsParser {
 
-    std::ifstream file(filename);
+public:
 
-    std::string line;
+    static bool validate(const std::string& cnf);
+};
 
-    while (std::getline(file, line)) {
-
-        if (line.empty()) continue;
-
-        if (line[0] == 'c')
-            continue;
-
-        if (line[0] == 'p') {
-
-            std::stringstream ss(line);
-
-            std::string tmp;
-
-            int clauses;
-
-            ss >> tmp >> tmp >> formula.variables >> clauses;
-
-            continue;
-        }
-
-        std::stringstream ss(line);
-
-        Clause clause;
-
-        int literal;
-
-        while (ss >> literal) {
-
-            if (literal == 0)
-                break;
-
-            clause.literals.push_back(literal);
-        }
-
-        formula.clauses.push_back(clause);
-    }
-
-    return formula;
-}
+#endif

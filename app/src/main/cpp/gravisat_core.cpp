cfe3c65d0a6f17b9ad6d5a0fbc92c9756d8dbaa4
aryav106-sketch <aryav106@gmail.com>
2026-05-22 19:47:41 +0530

    Update gravisat_core.cpp

--- app/src/main/cpp/gravisat_core.cpp
+++ app/src/main/cpp/gravisat_core.cpp
@@ -1,13 +1,32 @@
 #include <jni.h>
 #include <string>
+#include "solver.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_gravisat_shield_MainActivity_stringFromJNI(
+Java_com_gravisat_shield_MainActivity_solveSAT(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject,
+        jstring input) {
 
-    std::string hello = "GraviSAT v3.1 Native Engine";
+    const char* rawText =
+            env->GetStringUTFChars(input, nullptr);
 
-    return env->NewStringUTF(hello.c_str());
+    std::string cnf(rawText);
+
+    env->ReleaseStringUTFChars(input, rawText);
+
+    Solver solver;
+
+    bool result = solver.solve(cnf);
+
+    std::string output;
+
+    if (result) {
+        output = "SATISFIABLE";
+    } else {
+        output = "UNSATISFIABLE";
+    }
+
+    return env->NewStringUTF(output.c_str());
 }

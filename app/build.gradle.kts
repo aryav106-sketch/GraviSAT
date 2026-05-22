3eb5d56323ab4e4b28896c894bfc9ad46c3bc629
aryav106-sketch <aryav106@gmail.com>
2026-05-22 16:21:05 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,50 +4,70 @@ plugins {
 }
 
 android {
+
     namespace = "com.gravisat.shield"
+
     compileSdk = 34
 
     defaultConfig {
+
         applicationId = "com.gravisat.shield"
+
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 3
+        versionName = "3.0"
+
+        externalNativeBuild {
+
+            cmake {
+
+                cppFlags += ""
+            }
+        }
     }
 
     buildTypes {
+
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+
+            isMinifyEnabled = false
         }
+
         debug {
+
             isMinifyEnabled = false
         }
     }
 
     externalNativeBuild {
+
         cmake {
+
             path = file("src/main/cpp/CMakeLists.txt")
+
             version = "3.22.1"
         }
     }
 
+    ndkVersion = "25.1.8937393"
+
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
+
         jvmTarget = "17"
     }
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }

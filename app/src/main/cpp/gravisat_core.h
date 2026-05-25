0c17000f26cc897658e7f011b73ddbcfe957aec2
aryav106-sketch <aryav106@gmail.com>
2026-05-25 22:05:01 +0530

    Create gravisat_core.h

---
+++ app/src/main/cpp/gravisat_core.h
@@ -0,0 +1,23 @@
+#ifndef GRAVISAT_CORE_H
+#define GRAVISAT_CORE_H
+
+#include <string>
+#include "solver_state.h"
+
+class GraviSATCore {
+
+public:
+
+    SolverState state;
+
+    GraviSATCore();
+
+    bool solve();
+
+    void reset();
+
+    std::string getResult();
+
+};
+
+#endif

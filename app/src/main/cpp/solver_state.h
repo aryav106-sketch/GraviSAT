2709492b6fdb49e729dfcf4305191d6a5f9bf558
aryav106-sketch <aryav106@gmail.com>
2026-05-25 22:13:31 +0530

    Update solver_state.h

--- app/src/main/cpp/solver_state.h
+++ app/src/main/cpp/solver_state.h
@@ -4,18 +4,35 @@
 #include <vector>
 #include <cstdint>
 
+struct Variable {
+
+    int value;
+    bool seen;
+
+    Variable() {
+        value = -1;
+        seen = false;
+    }
+};
+
 struct SolverState {
 
     int vars;
     int clauses;
 
+    int currentLevel;
+
+    std::vector<Variable> vars_data;
+
     std::vector<int> assignment;
-    std::vector<int> decision_level;
     std::vector<int> trail;
+    std::vector<int> decision_level;
 
     SolverState() {
+
         vars = 0;
         clauses = 0;
+        currentLevel = 0;
     }
 };
 

d06823b818faf783a63645867cae88c8b28c4c14
aryav106-sketch <aryav106@gmail.com>
2026-05-25 22:14:51 +0530

    Update jni_bridge.cpp

--- app/src/main/cpp/jni_bridge.cpp
+++ app/src/main/cpp/jni_bridge.cpp
@@ -1,62 +1,16 @@
 #include <jni.h>
 #include <string>
-
 #include "gravisat_core.h"
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_gravisat_MainActivity_solveSAT(
-    JNIEnv* env,
-    jobject /* this */,
-    jstring inputText
-) {
+        JNIEnv* env,
+        jobject /* this */) {
 
-    if (inputText == nullptr) {
+    GraviSATCore solver;
 
-        return env->NewStringUTF(
-            "NULL INPUT");
-    }
+    std::string result = solver.getResult();
 
-    const char* rawText =
-        env->GetStringUTFChars(
-            inputText,
-            nullptr);
-
-    if (rawText == nullptr) {
-
-        return env->NewStringUTF(
-            "JNI STRING ERROR");
-    }
-
-    std::string cnfText(
-        rawText);
-
-    env->ReleaseStringUTFChars(
-        inputText,
-        rawText);
-
-    std::string result;
-
-    try {
-
-        result =
-            solveCNF(
-                cnfText);
-    }
-    catch (
-        const std::exception& e) {
-
-        result =
-            "NATIVE EXCEPTION: ";
-
-        result += e.what();
-    }
-    catch (...) {
-
-        result =
-            "UNKNOWN NATIVE ERROR";
-    }
-
-    return env->NewStringUTF(
-        result.c_str());
+    return env->NewStringUTF(result.c_str());
 }

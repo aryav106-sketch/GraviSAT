5a856b5184ab0c9d2160d3b0b996111f739ca96a
aryav106-sketch <aryav106@gmail.com>
2026-05-25 22:49:44 +0530

    Update solver.h

--- app/src/main/cpp/solver.h
+++ app/src/main/cpp/solver.h
@@ -2,36 +2,16 @@
 #define SOLVER_H
 
 #include <vector>
-#include <queue>
-#include <cmath>
+#include <string>
 
-struct Clause;
+struct Clause {
+
+    std::vector<int> literals;
+};
 
 struct Variable {
 
     int value = -1;
-
-    int level = 0;
-
-    float activity = 0.0f;
-
-    bool seen = false;
-
-    Clause* reason = nullptr;
-};
-
-struct TrailEntry {
-
-    int lit;
-};
-
-struct Clause {
-
-    std::vector<int> lits;
-
-    float activity = 0.0f;
-
-    bool learnt = false;
 };
 
 struct SolverState {
@@ -40,21 +20,9 @@ struct SolverState {
 
     std::vector<Clause> clauses;
 
-    std::vector<TrailEntry> trail;
-
-    std::vector<std::vector<int>> watchLists;
-
-    std::queue<int> propagationQueue;
-
     int conflicts = 0;
 
     int decisions = 0;
-
-    int restarts = 0;
-
-    float varDecay = 0.95f;
-
-    float clauseDecay = 0.999f;
 };
 
 #endif
